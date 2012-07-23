070aeab78840618929582c43f40cd5a4f8454a0a
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2012-07-23 17:49:45 +0400

    Fix compilation on jellybean

--- samsung-ril-client.c
+++ samsung-ril-client.c
@@ -78,7 +78,7 @@ int srs_send(int *p_client_fd, unsigned short command, void *data, int data_len)
 	struct srs_message message;
 	int rc;
 
-	LOGE("%s", __func__);
+	ALOGE("%s", __func__);
 
 	message.command = command;
 	message.data = data;
@@ -156,10 +156,10 @@ int srs_ping(int *p_client_fd)
 	caffe_r = *((int *) message.data);
 
 	if(caffe_r == SRS_CONTROL_CAFFE) {
-		LOGD("Caffe is ready!");
+		ALOGD("Caffe is ready!");
 		rc = 0;
 	} else {
-		LOGE("Caffe went wrong!");
+		ALOGE("Caffe went wrong!");
 		rc = -1;
 	}
 
@@ -171,16 +171,16 @@ int OpenClient_RILD(void)
 {
 	int *client_fd_p = NULL;
 
-	LOGE("%s", __func__);
+	ALOGE("%s", __func__);
 
 	signal(SIGPIPE, SIG_IGN);
 
 	client_fd_p = malloc(sizeof(int));
 	if (!client_fd_p) {
-		LOGE("%s: failed to allocate memory for the client fd", __func__);
+		ALOGE("%s: failed to allocate memory for the client fd", __func__);
 		return 0;
 	}
-	LOGE("%s: client fd pointer %x", __func__, client_fd_p);
+	ALOGE("%s: client fd pointer %x", __func__, client_fd_p);
 	*client_fd_p = -1;
 
 	return client_fd_p;
@@ -193,38 +193,38 @@ int Connect_RILD(int *pfd)
 	int rc;
 	int *client_fd_p = pfd;
 
-	LOGE("%s", __func__);
+	ALOGE("%s", __func__);
 
 socket_connect:
 	while(t < 5) {
 		fd = socket_local_client(SRS_SOCKET_NAME, ANDROID_SOCKET_NAMESPACE_ABSTRACT, SOCK_STREAM);
-		LOGE("%s: fd %d, errno %d, err %s", __func__, fd, errno, strerror(errno));
+		ALOGE("%s: fd %d, errno %d, err %s", __func__, fd, errno, strerror(errno));
 
 		if(fd >= 0)
 			break;
 
-		LOGE("Socket creation to RIL failed: trying another time");
+		ALOGE("Socket creation to RIL failed: trying another time");
 
 		t++;
 		usleep(300);
 	}
 
 	if(fd < 0) {
-		LOGE("Socket creation to RIL failed too many times");
+		ALOGE("Socket creation to RIL failed too many times");
 		return RIL_CLIENT_ERR_CONNECT;
 	}
 
 	*client_fd_p = fd;
 
-	LOGE("Socket creation done, sending ping");
+	ALOGE("Socket creation done, sending ping");
 
 	rc = srs_ping(pfd);
 
 	if(rc < 0) {
-		LOGE("Ping failed!");
+		ALOGE("Ping failed!");
 		goto socket_connect;
 	} else {
-		LOGD("Ping went alright");
+		ALOGD("Ping went alright");
 	}
 
 	return RIL_CLIENT_ERR_SUCCESS;
@@ -234,7 +234,7 @@ int Disconnect_RILD(int *p_client_fd)
 {
 	int client_fd = *p_client_fd;
 
-	LOGE("%s", __func__);
+	ALOGE("%s", __func__);
 
 	close(client_fd);
 
@@ -243,7 +243,7 @@ int Disconnect_RILD(int *p_client_fd)
 
 int CloseClient_RILD(int *pfd)
 {
-	LOGE("%s", __func__);
+	ALOGE("%s", __func__);
 	free(pfd);
 
 	return RIL_CLIENT_ERR_SUCCESS;
@@ -254,8 +254,8 @@ int isConnected_RILD(int *pfd)
 	int client_fd;
 	int rc;
 
-	LOGE("%s", __func__);
-	LOGE("%s: pfd %x", __func__, pfd);
+	ALOGE("%s", __func__);
+	ALOGE("%s: pfd %x", __func__, pfd);
 
 	client_fd = *pfd;
 	if(client_fd < 0) {
@@ -265,12 +265,12 @@ int isConnected_RILD(int *pfd)
 	rc = srs_ping(pfd);
 
 	if(rc < 0) {
-		LOGE("Ping failed!");
+		ALOGE("Ping failed!");
 		close(client_fd);
 
 		return 0;
 	} else {
-		LOGD("Ping went alright");
+		ALOGD("Ping went alright");
 	}
 
 	return 1;
@@ -280,7 +280,7 @@ int SetCallVolume(int *pfd, SoundType type, int vol_level)
 {
 	struct srs_snd_call_volume call_volume;
 
-	LOGD("Asking call volume");
+	ALOGD("Asking call volume");
 
 	call_volume.type = (enum srs_snd_type) type;
 	call_volume.volume = vol_level;	
@@ -295,7 +295,7 @@ int SetCallAudioPath(int *pfd, AudioPath path)
 {
 	srs_send(pfd, SRS_SND_SET_CALL_AUDIO_PATH, (void *) &path, sizeof(enum srs_snd_path));
 
-	LOGD("Asking audio path");
+	ALOGD("Asking audio path");
 
 	return RIL_CLIENT_ERR_SUCCESS;
 }
@@ -304,7 +304,7 @@ int SetCallClockSync(int *pfd, SoundClockCondition condition)
 {
 	unsigned char data = condition;
 
-	LOGD("Asking clock sync");
+	ALOGD("Asking clock sync");
 
 	srs_send(pfd, SRS_SND_SET_CALL_CLOCK_SYNC, &data, sizeof(data));
 
